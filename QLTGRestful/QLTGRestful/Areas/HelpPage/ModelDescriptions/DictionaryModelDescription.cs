81b1461fe2ef0a2910146328a0700656aa39f97e
Nguyen Tai Hanh <taihanh0310@gmail.com>
2016-05-29 15:10:58 +0700

    clear

--- QLTGRestful/QLTGRestful/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace QLTGRestful.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
---
+++ ptudweb/oopjs/beTapTinh.js
@@ -0,0 +1,27 @@
+var message = "";
+function tinh(soA, soB, operator) {
+	switch(operator){
+		case "+":{
+			break;
+		}
+		case "-": {
+			break;
+		}
+		case "x":{
+			break;
+		}
+		case "/": {
+			break;
+		}
+		default:{
+			break;
+		}
+	}
+}
+
+function checkIsNumber(argument) {
+	if(!isNaN(argument)){
+		return false;
+	}
+	return true;
+}
