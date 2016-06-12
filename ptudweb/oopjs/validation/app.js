d9fa5899f398aacb829414e8b9d6aaeef0f0c09a
Hanh Nguyen Tai <taihanh0310@gmail.com>
2016-06-12 16:30:47 +0700

    tested

--- ptudweb/oopjs/validation/app.js
+++ ptudweb/oopjs/validation/app.js
@@ -1,9 +1,17 @@
-var validationApp = angular.module('validationApp', []);
+// create angular app
+    var validationApp = angular.module('validationApp', []);
 
-validationApp.controller('mainController', function($scope){
-	$scope.submitForm = function(isVaild){
-		if (isVaild) {
-			alert('our form is success');
-		}
-	};
-});
+    // create angular controller
+    validationApp.controller('mainController', function($scope) {
+
+        // function to submit the form after all validation has occurred            
+        $scope.submitForm = function() {
+
+            // check to make sure the form is completely valid
+            if ($scope.userForm.$valid) {
+                alert('our form is amazing');
+            }
+
+        };
+
+    });
